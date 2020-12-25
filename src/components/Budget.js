956572277f5501d9d11e3a2d353875d324e06448
Mritunjoy <mritunjoy.mahanta001@gmail.com>
2020-12-25 10:58:46 +0530

    created the basic components

---
+++ src/components/Budget.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Budget() {
+  return <div>Hello from Single Element Budget</div>;
+}
---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Budget from "./Budget";
+
+export default function List() {
+  return (
+    <div>
+      Hello from Budget List
+      <Budget />
+    </div>
+  );
+}
